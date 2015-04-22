64133302c69fcc5e84386b5b54bea2be810b74e6
Daniel Sipka <no1msd@gmail.com>
2015-04-22 12:01:38 +0200

    remove incomplete code

--- src/visitor/render_section.hpp
+++ src/visitor/render_section.hpp
@@ -30,14 +30,13 @@ namespace mstch {
 
         template<> inline
         std::string render_section::operator()<lambda>(const lambda& lam) const {
-            std::string section_str;
+            return "";
+            /*std::string section_str;
             for(auto& token: section)
                 section_str += token.raw();
             return lam(section_str, [this](const std::string& str) {
-                std::cout << str << std::endl;
-                std::cout << ctx.render(template_type{str}) << std::endl;
                 return ctx.render(template_type{str});
-            });
+            });*/
         }
 
         template<> inline
