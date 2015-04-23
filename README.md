c4b5eb8664c6a9508cf30a261685437ba37a8629
Daniel Sipka <no1msd@gmail.com>
2015-04-24 01:37:09 +0200

    update readme

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ int main() {
   std::string view{"{{#names}}Hi {{name}}!\n{{/names}}"};
   mstch::map context{
     {"names", mstch::array{
-      mstch::map{{"name", std::string{"Chris"}},
-      mstch::map{{"name", std::string{"Mark"}},
-      mstch::map{{"name", std::string{"Scott"}},
-    }
+      mstch::map{{"name", std::string{"Chris"}}},
+      mstch::map{{"name", std::string{"Mark"}}},
+      mstch::map{{"name", std::string{"Scott"}}},
+    }}
   };
   
   std::cout << mstch::render(view, context) << std::endl;
--- test/benchmark_main.cpp
+++ test/benchmark_main.cpp
@@ -1,42 +1,18 @@
-#include "mstch/mstch.hpp"
-
-#include <chrono>
 #include <iostream>
 
-unsigned long current_msec() {
-  return std::chrono::system_clock::now().time_since_epoch() /
-      std::chrono::milliseconds(1);
-}
+#include <mstch/mstch.hpp>
 
 int main() {
-  std::string comment_tmp{
-      "<div class=\"comments\"><h3>{{header}}</h3><ul>"
-      "{{#comments}}<li class=\"comment\"><h5>{{name}}</h5>"
-      "<p>{{body}}</p></li>{{/comments}}</ul></div>"};
+    std::string view{"{{#names}}Hi {{name}}!\n{{/names}}"};
+    mstch::map context{
+        {"names", mstch::array{
+            mstch::map{{"name", std::string{"Chris"}}},
+            mstch::map{{"name", std::string{"Mark"}}},
+            mstch::map{{"name", std::string{"Scott"}}},
+        }}
+    };
 
-  auto comment_view = mstch::map{
-      {"header", std::string{"My Post Comments"}},
-      {"comments", mstch::array{
-          mstch::map{{"name", std::string{"Joe"}}, {"body", std::string{"Thanks for this post!"}}},
-          mstch::map{{"name", std::string{"Sam"}}, {"body", std::string{"Thanks for this post!"}}},
-          mstch::map{{"name", std::string{"Heather"}}, {"body", std::string{"Thanks for this post!"}}},
-          mstch::map{{"name", std::string{"Kathy"}}, {"body", std::string{"Thanks for this post!"}}},
-          mstch::map{{"name", std::string{"George"}}, {"body", std::string{"Thanks for this post!"}}}}}};
+            std::cout << mstch::render(view, context) << std::endl;
 
-  std::vector<unsigned long> times;
-  for (int j = 0; j < 10; j++) {
-    unsigned long start = current_msec();
-    for (int i = 0; i < 5000; i++)
-      mstch::render(comment_tmp, comment_view);
-    times.push_back(current_msec() - start);
-  }
-
-  float avg = 0;
-  for (auto i: times)
-    avg += i;
-  avg /= times.size();
-
-  std::cout << avg << std::endl;
-
-  return 0;
-}
+            return 0;
+    }
